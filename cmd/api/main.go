92a8161585e783b80d0d1b84eb3e9c62f12f1127
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2025-01-01 01:52:19 +0500

    implementing repository pattern for application

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,14 +4,17 @@ import (
 	"log"
 
 	"github.com/supremed3v/social-media/internal/env"
+	"github.com/supremed3v/social-media/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 	}
+	store := store.NewStorage(nil)
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
