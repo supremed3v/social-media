ab1cbf55ece0f5f30df24389ab9c2073ca2cc93b
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2025-01-01 11:08:18 +0500

    working on user feed posts

---
+++ cmd/api/posts.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/supremed3v/social-media/internal/store"
+)
+
+type CreatePostPayload struct {
+	Title   string   `json:"title"`
+	Content string   `json:"content"`
+	Tags    []string `json:"tags"`
+}
+
+func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
+
+	var payload CreatePostPayload
+
+	if err := readJSON(w, r, &payload); err != nil {
+		writeJSONError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	userId := 1
+
+	post := &store.Post{
+		Title:   payload.Title,
+		Content: payload.Content,
+		Tags:    payload.Tags,
+		// TODO: Change after auth
+		UserID: int64(userId),
+	}
+
+	ctx := r.Context()
+
+	if err := app.store.Posts.Create(ctx, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
