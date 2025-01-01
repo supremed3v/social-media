e97b7a623bcbf91db527fa014b6290ca64657ae6
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2025-01-01 09:28:54 +0500

    configured the db connection pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/supremed3v/social-media/internal/db"
 	"github.com/supremed3v/social-media/internal/env"
 	"github.com/supremed3v/social-media/internal/store"
 )
@@ -10,8 +11,25 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
-	store := store.NewStorage(nil)
+
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+
+	log.Printf("db connected")
+
+	store := store.NewStorage(db)
 	app := &application{
 		config: cfg,
 		store:  store,
