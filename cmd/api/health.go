4e2f100c5a375b9184b3c3dad3c9b92defd4b51e
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2025-01-02 07:39:16 +0500

    removed log and implemented internal errors package to data layer

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -15,8 +15,7 @@ func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	if err := writeJSON(w, http.StatusOK, data); err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		app.internalServerError(w, r, err)
 	}
 
-	// app.store.Posts.Create(r.Context(), &store.Post{})
 }
