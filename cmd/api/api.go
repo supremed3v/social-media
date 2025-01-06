61d766e841a6ee41eff17e26822f966c27fae783
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2025-01-06 05:34:23 +0500

    working on user profile module

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -56,6 +56,11 @@ func (app *application) mount() http.Handler {
 				})
 			})
 		})
+		r.Route("/users", func(r chi.Router) {
+			r.Route("/{userID}", func(r chi.Router) {
+				r.Get("/", app.getUserHandler)
+			})
+		})
 
 	})
 	return r
--- internal/store/users.go
+++ internal/store/users.go
@@ -33,3 +33,25 @@ func (s *UserStore) Create(ctx context.Context, user *User) error {
 	return nil
 
 }
+
+func (s *UserStore) GetByID(ctx context.Context, userID int64) (*User, error) {
+	query := `
+		SELECT id, username, email, password,createdAt 
+		FROM users 
+		where ID = $1
+	`
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+	user := &User{}
+	err := s.db.QueryRowContext(ctx, query, userID).Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.CreatedAt)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return nil, ErrNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return user, nil
+}
