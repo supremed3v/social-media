6911a79bd16e049c19ea877e745e48c72dc7922f
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2025-01-09 09:22:49 +0500

    implemented pagination an sorting

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -142,7 +142,7 @@ func (s *PostStore) Update(ctx context.Context, post *Post) error {
 	return nil
 }
 
-func (s *PostStore) GetUserFeed(ctx context.Context, userID int64) ([]PostWithMetadata, error) {
+func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedFeedQuery) ([]PostWithMetadata, error) {
 	query := `
 		SELECT 
 		p.id, p.user_id, p.title, p.content, p.createdat, p.version, p.tags, u.username,COUNT(c.id) AS comments_count
@@ -152,12 +152,13 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64) ([]PostWithMe
 		JOIN followers f ON f.follower_id = p.user_id OR p.user_id = $1
 		WHERE f.user_id = $1 or p.user_id = $1
 		GROUP BY p.id, u.username
-		ORDER BY p.createdat DESC 
+		ORDER BY p.createdat ` + fq.Sort + `
+		LIMIT $2 OFFSET $3
 	`
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
 	defer cancel()
 
-	rows, err := s.db.QueryContext(ctx, query, userID)
+	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset)
 	if err != nil {
 		return nil, err
 	}
