7c33c38f4daa219760d3d1bf9b2432a5227c4670
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2025-01-09 07:15:32 +0500

    enhanced migrations

--- cmd/migrate/migrations/000003_alter_post_table.up.sql
+++ cmd/migrate/migrations/000003_alter_post_table.up.sql
@@ -1,4 +1,13 @@
-ALTER TABLE
- posts
-ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users
-(id);
+DO $$ 
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.table_constraints
+        WHERE constraint_type = 'FOREIGN KEY'
+        AND table_name = 'posts'
+        AND constraint_name = 'fk_user'
+    ) THEN
+        ALTER TABLE posts
+        ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id);
+    END IF;
+END $$;
--- cmd/migrate/migrations/000004_alter_posts_with_tags_updated.up.sql
+++ cmd/migrate/migrations/000004_alter_posts_with_tags_updated.up.sql
@@ -1,3 +1,20 @@
-ALTER TABLE posts
-ADD COLUMN tags VARCHAR(100) [];
-ALTER TABLE posts ADD COLUMN updatedAt timestamp(0) with time zone NOT NULL DEFAULT NOW()
+DO $$ 
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM information_schema.columns 
+        WHERE table_name = 'posts' 
+        AND column_name = 'tags'
+    ) THEN
+        ALTER TABLE posts ADD COLUMN tags VARCHAR(100);
+    END IF;
+
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM information_schema.columns 
+        WHERE table_name = 'posts' 
+        AND column_name = 'updatedat'
+    ) THEN
+        ALTER TABLE posts ADD COLUMN updatedAt TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW();
+    END IF;
+END $$;
