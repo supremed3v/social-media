626f50f02e692d3dc50fc01ce27991dd1ddea08c
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2025-01-21 05:40:56 +0500

    implemented basic server metrics

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"expvar"
 	"log"
+	"runtime"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -126,6 +128,16 @@ func main() {
 		authenticator: jwtAuthenticator,
 		rateLimiter:   rateLimiter,
 	}
+
+	// Metrics collected
+
+	expvar.NewString("version").Set(version)
+	expvar.Publish("database", expvar.Func(func() any {
+		return db.Stats()
+	}))
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
 
 	mux := app.mount()
 
